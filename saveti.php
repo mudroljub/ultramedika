8ca2b7410a4a462d3b6cd442d7868bf92e2cfbc5
mudroljub <mudroljub@gmail.com>
2025-02-09 18:19:46 +0100

    SAVETI

--- saveti.php
+++ saveti.php
@@ -7,8 +7,39 @@ include_once "ukljuci/header.php";
 
       <h2>2024. godina</h2>
 
-      <a href="saveti/Mamografija 1.docx">Mamografija</a>
-
-</div>
+      <a href="saveti/2024/ANEURIZMA ABDOMINALNE AORTE.docx">ANEURIZMA ABDOMINALNE AORTE</a>
+      <a href="saveti/2024/BIOPSIJA PROSTATE-POVIŠEN PSA.docx">BIOPSIJA PROSTATE-POVIŠEN PSA</a>
+      <a href="saveti/saveti/2024/BIOPSIJA ŠTITNE ŽLEZDE.docx">BIOPSIJA ŠTITNE ŽLEZDE</a>
+      <a href="saveti/2024/DIJABET TIP 2-METFORMIN.docx">DIJABET TIP 2-METFORMIN</a>
+      <a href="saveti/2024/EKLAMPSIJA.docx">EKLAMPSIJA</a>
+      <a href="saveti/2024/Endometriosis Quadruples Ovarian Cancer Risk.docx">Endometriosis Quadruples Ovarian Cancer Risk</a>
+      <a href="saveti/2024/HORMONSKA TERAPIJA-MENOPAUZA.docx">HORMONSKA TERAPIJA-MENOPAUZA</a>
+      <a href="saveti/2024/KARCINOM GRLICA MATERICE.docx">KARCINOM GRLICA MATERICE</a>
+      <a href="saveti/2024/KARCINOM JAJNIKA.docx">KARCINOM JAJNIKA</a>
+      <a href="saveti/2024/KARCINOM PROSTATE DA ILI NE.docx">KARCINOM PROSTATE DA ILI NE</a>
+      <a href="saveti/2024/KARCINOM STITNE ZLEZDE-ULTRAZVUK.docx">KARCINOM STITNE ZLEZDE-ULTRAZVUK</a>
+      <a href="saveti/2024/KOLONOSKOPIJA ILI.docx">KOLONOSKOPIJA ILI</a>
+      <a href="saveti/2024/Mamografija 1.docx">Mamografija 1</a>
+      <a href="saveti/2024/MAMOGRAFIJA, ULTRAZVUK, VESTACKA INTELIGENCIJA.docx">MAMOGRAFIJA, ULTRAZVUK, VESTACKA INTELIGENCIJA</a>
+      <a href="saveti/2024/MAMOGRAFIJA.docx">MAMOGRAFIJA</a>
+      <a href="saveti/2024/MATERIČNA ILI VANMATERIČNA TRUDNOĆA.docx">MATERIČNA ILI VANMATERIČNA TRUDNOĆA</a>
+      <a href="saveti/2024/OSTEOPOROZA.docx">OSTEOPOROZA</a>
+      <a href="saveti/2024/PCOS.docx">PCOS</a>
+      <a href="saveti/2024/Peripartum Prediction of Fetal Weight in Gravidas With Obesity.docx">Peripartum Prediction of Fetal Weight in Gravidas With Obesity</a>
+      <a href="saveti/2024/PREEKLAMPSIJA.docx">PREEKLAMPSIJA</a>
+      <a href="saveti/2024/PROSTATA.docx">PROSTATA</a>
+      <a href="saveti/2024/RAK GRLIĆA MATERICE.docx">RAK GRLIĆA MATERICE</a>
+      <a href="saveti/2024/SLEPO CREVO.docx">SLEPO CREVO</a>
+      <a href="saveti/2024/TRUDNICE PAŽNJA.docx">TRUDNICE PAŽNJA</a>
+      <a href="saveti/2024/TRUDNOCA 4D.docx">TRUDNOCA 4D</a>
+      <a href="saveti/2024/TRUDNOĆA MALROTACIJA CREVA.docx">TRUDNOĆA MALROTACIJA CREVA</a>
+      <a href="saveti/2024/TUMORI JAJNIKA.docx">TUMORI JAJNIKA</a>
+      <a href="saveti/2024/ULTRAZVUK DEBELOG CREVA.docx">ULTRAZVUK DEBELOG CREVA</a>
+      <a href="saveti/2024/ULTRAZVUK MALE KARLICE.docx">ULTRAZVUK MALE KARLICE</a>
+      <a href="saveti/2024/ULTRAZVUK ŽELUDCA.docx">ULTRAZVUK ŽELUDCA</a>
+      <a href="saveti/2024/URODJENA HIDRONEFROZA BUBREGA.docx">URODJENA HIDRONEFROZA BUBREGA</a>
+      <a href="saveti/2024/URODJENE SRCANE MANE-ULTRAZVUK.docx">URODJENE SRCANE MANE-ULTRAZVUK</a>
+      <a href="saveti/2024/МАМОГРАФИЈА ili ULTRAZVUK.docx">МАМОГРАФИЈА ili ULTRAZVUK</a>
+  </div>
 
 <?php include_once "ukljuci/footer.php";?>
